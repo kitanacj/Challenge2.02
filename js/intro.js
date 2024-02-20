31fce73dac0fd8de00cb661c404a4fc925e6fbf8
kitanacj <kitancj14@gmail.com>
2024-02-19 18:47:22 -0800

    Creating box and toggle with button

--- js/intro.js
+++ js/intro.js
@@ -3,44 +3,42 @@
 $(document).ready(function(){
 
 
-    $("#cat-img").hide();
-    $("#happy-dog").hide();
-    $("#secret-text").hide();
+    $("#box-empty").hide();
 
-    $("#doggo-img").click(function(){
-        $("#doggo-img").hide();
-        $("#cat-img").show();
+    // $("#doggo-img").click(function(){
+    //     $("#doggo-img").hide();
+    //     $("#cat-img").show();
 
 
-    });
+    // });
 
-    $("#cat-img").click(function(){
-        // $("#doggo-img").show();
-        $("#cat-img").hide();
-        $("#happy-dog").show();
-
-
-    });
-
-    $("#happy-dog").click(function(){
-        $("#doggo-img").show();
-        $("#cat-img").hide();
-        $("#happy-dog").hide();
+    // $("#cat-img").click(function(){
+    //     // $("#doggo-img").show();
+    //     $("#cat-img").hide();
+    //     $("#happy-dog").show();
 
 
-    });
+    // });
+
+    // $("#happy-dog").click(function(){
+    //     $("#doggo-img").show();
+    //     $("#cat-img").hide();
+    //     $("#happy-dog").hide();
+
+
+    // });
 
     $("#toggle-text").click(function(){
-        $("#secret-text").toggle();
+        $("#box-empty").toggle();
         
     });
 
-    $("#change-bg-color").click(function(){
-        $("#home-screen").css("background-color", "khaki");
-        $("#change-bg-color").css("color", "white");
+    // $("#change-bg-color").click(function(){
+    //     $("#home-screen").css("background-color", "khaki");
+    //     $("#change-bg-color").css("color", "white");
 
         
-    });
+    // });
 
 
 });
