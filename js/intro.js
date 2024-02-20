28704b5d151e1b3f194959f1e071b2e6d8c1c5e3
kitanacj <kitancj14@gmail.com>
2024-02-19 18:15:12 -0800

    Js images for examples

--- js/intro.js
+++ js/intro.js
@@ -1,4 +1,23 @@
 //resource link: https://www.w3schools.com/jquery/default.asp
 
-$(document).ready(function)
+$(document).ready(function(){
+
+
+    $("#cat-img").hide();
+
+    $("#doggo-img").click(function(){
+        $("#doggo-img").hide();
+        $("#cat-img").show();
+
+
+    });
+
+    $("#cat-img").click(function(){
+        $("#doggo-img").show();
+        $("#cat-img").hide();
+
+
+    });
+
+});
 
